703eab127371ea47bea694c4296b2b03abaedce1
jeeva-freezee <156514981+redhodom@users.noreply.github.com>
2026-07-06 13:30:39 +0530

    updated resume added

--- src/home.jsx
+++ src/home.jsx
@@ -10,7 +10,7 @@ import logo from "/jflogo.png"
 import { useState, useEffect } from "react"
 import { FaJava, FaReact, FaNodeJs, FaGit, FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiSpringboot, SiMysql, SiJavascript } from "react-icons/si";
-import resume from "/JDFSD.pdf";
+import resume from "/FsJeevaResume.pdf";
 
 
 const Home = () => {
